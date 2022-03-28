0899de89e131598950df72396a680db06d860e10
Devon Call <callbrentdevon@gmail.com>
2022-03-28 16:01:42 -0700

    improve readability on fuzz test

--- fuzz_test.go
+++ fuzz_test.go
@@ -19,21 +19,22 @@ func FuzzCopy(f *testing.F) {
 		f.Add(seed.height, seed.width, seed.start_real, seed.start_imag, seed.step_real, seed.step_imag)
 	}
 	f.Fuzz(func(t *testing.T, height int, width int, start_real float64, start_imag float64, step_real float64, step_imag float64) {
-		if height > 0 && width > 0 {
-			m := make(immutable, width)
-			val := complex(start_real, start_imag)
-			step := complex(step_real, step_imag)
-			for c := range m {
-				m[c] = make([]complex128, height)
-				for r := range m[c] {
-					m[c][r] = val
-					val += step
-				}
+		if height <= 0 || width <= 0 {
+			t.Skip("invalid dimensions")
+		}
+		m := make(immutable, width)
+		val := complex(start_real, start_imag)
+		step := complex(step_real, step_imag)
+		for c := range m {
+			m[c] = make([]complex128, height)
+			for r := range m[c] {
+				m[c][r] = val
+				val += step
 			}
-			o := m.copy()
-			if !Equal(o, m) {
-				t.Fatalf("Copy failed: %v, %v", m, o)
-			}
+		}
+		o := m.copy()
+		if !Equal(o, m) {
+			t.Fatalf("Copy failed: %v, %v", m, o)
 		}
 	})
 }
